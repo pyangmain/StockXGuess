93273b21201a6d0d4984638adefc34291efd167f
peteryang <pyangmain@gmail.com>
2024-01-17 19:33:14 -0500

    finished css and bugfixing

--- src/main/javascript/sneaker-frontend/src/App.js
+++ src/main/javascript/sneaker-frontend/src/App.js
@@ -20,6 +20,8 @@ export default function Game() {
       setSneakerArray(data);
       setRound(1);
       setScore(0);
+      setGuessInput('')
+      setPointsGained(0)
     }).then(() => {
       setGameState("display");
     })
@@ -29,14 +31,14 @@ export default function Game() {
   const handleSubmit = (e) => {
     e.preventDefault();
     var userGuess;
-    if(isNaN(guessInput)) {
+    if(guessInput === "") {
       userGuess = 0;
+      setGuessInput("0");
     }
     else {
       userGuess = parseInt(guessInput);
     }
-    var gainedPoints = Math.max(0, Math.floor(1000 - Math.pow(Math.abs(parseInt(userGuess) - sneakerArray[round - 1].price), 1.38)));
-    console.log(gainedPoints);
+    var gainedPoints = Math.max(0, Math.floor(1000 - Math.pow(Math.abs(parseInt(userGuess) - sneakerArray[round - 1].price), 1.6)));
     setPointsGained(gainedPoints);
     setScore(score + gainedPoints);
     setGameState("result");
@@ -56,11 +58,13 @@ export default function Game() {
   function renderStart() {
     return (
       <div className="centered-div">
-                <h1>Stock</h1>
-                <h1 className="stockXgreen">XGame</h1> 
+                <h4>
+                  <span>Stock</span>
+                  <span className="stockXgreen">XGame</span>
+                </h4>
                 <p>How well do you know your sneakers? Test out your shoe knowledge by pressing play below.</p>
-                <p className="QUICKSAND">Here's how the game works: We give you 10 random shoes on StockX, and you have to guess the price. The closer the guess to the average selling price, the more points you get.</p>
-                <p>Idea inspired from <a href="https://www.guessense.com/">GUESSENSE.</a></p>
+                <p className="QUICKSAND">We give you 10 random shoes on StockX, and you have to guess the price. The closer the guess to the average selling price, the more points you get.</p>
+                <p>Idea inspired from <a href="https://guessense.nkdr.ca/">GUESSENSE.</a></p>
                 <div className="centered-div">
                     <button id="start-button" className="green-button" onClick={initGame}>Start</button>
                 </div>
@@ -116,10 +120,10 @@ export default function Game() {
             <h1 id="score" className="score">SCORE: {score}</h1>
         </div>
         <div id="text-div" className="text-div">
-            <a href={sneakerArray[round - 1].sneakerUrl} id="product-link"><h2 id="product-text">Link to shoes</h2></a>
-            <h2 id="shoe-price">{sneakerArray[round - 1].price}</h2>
-            <h2 id="player-answer">{guessInput}</h2>
-            <h2 id="points-gained">{pointsGained}</h2>
+            <a href={sneakerArray[round - 1].sneakerUrl} id="product-link"><h2 id="product-text">{sneakerArray[round - 1].sneakerName}</h2></a>
+            <h2 id="shoe-price">${sneakerArray[round - 1].price} USD</h2>
+            <h2 id="player-answer">Your Guess: {guessInput}</h2>
+            <h2 id="points-gained">Points Gained: {pointsGained}</h2>
         </div>
         <button id="guess-button" className="fixed-green-button" onClick={advanceRound}>Continue</button>
       </div>
@@ -128,8 +132,8 @@ export default function Game() {
 
   function renderEnd() {
     return (
-      <div>
-        <div id="final-message">
+      <div className='text-div'>
+        <div id="final-message" className="final-message-div">
                 <h2>GAME OVER</h2>
                 <h2 id="final-score">Final Score: {score}</h2>
             </div>
